1c803c29fcfe1265cf516e42a7b2c10b6bb5e62e
mateusbutchaka <mateusgotardi06@gmail.com>
2023-09-21 09:34:20 -0300

    deixando-as-pagina-dinamicas+json

---
+++ assets/script/productPage.js
@@ -0,0 +1,45 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(url);
+    return results ? decodeURIComponent(results[2].replace(/\+/g, ' ')) : null;
+}
+
+function loadProductDetails() {
+    const productId = getParameterByName('id');
+    if (!productId) {
+        console.error('ID do produto não encontrado na URL.');
+        return;
+    }
+
+    fetch('/json/produto.json')
+        .then((response) => response.json())
+        .then((data) => {
+            const product = data.find((item) => item.id === parseInt(productId));
+            if (product) {
+                const { nome, cor, descricao, img } = product;
+                document.getElementById('product-name').textContent = nome;
+                document.getElementById('product-color').textContent = `Cor: ${cor}`;
+                document.getElementById('product-description').textContent = descricao;
+                document.getElementById('product-image').src = img; 
+                        // colocar mais conteudo abaixo dependendo do fo rusa
+                                                                     // Trocar numero e colocar o da moca da loja 
+                const whatsappLink = "https://api.whatsapp.com/send?phone=554396632670&text=Olá, estou interessado no produto: " + encodeURIComponent(nome);
+                document.getElementById("contact-product").addEventListener("click", function() {
+                    window.location.href = whatsappLink;
+                });
+            } else {
+                console.error('Produto não encontrado');
+            }
+        })
+        .catch((error) => {
+            console.error('Erro ao carregar detalhes do produto:', error);
+        });
+}
+
+loadProductDetails();
+
+
+
+
+
